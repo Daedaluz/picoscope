b00e5757f81f1e5733475ed0b8c7630b853eda25
Tobias Assarsson <tobias.assarsson@gmail.com>
2021-09-28 17:33:27 +0200

    add xhost + and docker name

--- picoscope.sh
+++ picoscope.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+xhost +
+
 docker run --privileged \
 	--env=DISPLAY \
 	--user=$(id -u):$(id -g) \
@@ -7,6 +9,7 @@ docker run --privileged \
 	-v /sys:/sys \
 	-v /tmp/.X11-unix/:/tmp/.X11-unix:rw \
 	-v ~/.local:/.local \
+	--name picoscope \
 	--net=host \
 	--rm \
 	-ti \
